cb79ff937ae835db3dd47e5e331dd0efd7b4ef9e
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-02-22 07:23:02 -0500

    storing data to database

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const port = 3000
 
 connectToMongo();
 
+// middleware: needed to use req.body
+app.use(express.json())
+
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
   
