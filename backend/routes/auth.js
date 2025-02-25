99e9c38aba556dc16d879852b6785e8efa9af0f5
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-02-25 08:26:44 -0500

    added data validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,35 @@
 const router = require('express').Router();
 const User=require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 // create a user using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res) => {
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+    body('name','Enter a valid name').isLength({min:3}),
+    body('password','Password must be at least 5 characters').isLength({min:5})
+
+], (req, res) => {
     
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user=>{
+        res.send(user);
+    }).catch((error)=>{
+        {console.log(error)
+            res.json({error:"Please enter a unique email",message:error.message})}; 
+
+
+    });
 });
+    
+
+
 
 module.exports = router;
